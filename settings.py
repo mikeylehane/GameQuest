354b40b369b067dcaff8760969e9d09d4aab22a5
mikeylehane <62357543+mikeylehane@users.noreply.github.com>
2020-03-26 09:43:07 -0700

    color changes

---
+++ settings.py
@@ -0,0 +1,17 @@
+# game options/settings
+TITLE = "Mikey's First Game(Kinda)"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.01
+
+# define colors
+SILVER = (192, 192, 192)
+BLACK = (0, 0, 0)
+LIGHTRED = (255, 100, 100)
+PURPLE = (102, 0, 102)
+TEAL = (0, 128, 128)
+YELLOW = (255, 255, 0)
